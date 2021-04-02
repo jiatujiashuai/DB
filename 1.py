c5736d6053969bfbee0dc8e7589d13806625350f
jiatujiashuai <ywzhou_zjs@126.com>
2021-04-02 00:19:30 -0400

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,38 @@
+from db_table import db_table
+from sys import argv
+import os.path
+import pandas as pd
+import xlrd
+from sqlalchemy import create_engine
+
+
+filename = None
+
+def Parseexcel(fname):
+    df = pd.read_excel(
+        fname,
+        #sheet_name= "Agenda Database",
+        header = 14,
+        names=['Date', 'Time_Start', 'Time_End', 'Session', 'Session_Title', 'Room_Location', 'Description', 'Speakers'])
+    df.head()
+    print(df.columns)
+
+    users = db_table("Agenda", {"id": "integer PRIMARY KEY", "Date": "text NOT NULL", "Time_Start": "text NOT NULL", "Time_End": "text NOT NULL", "Session": "text NOT NULL", "Session_Title": "text NOT NULL", "Room_Location": "text", "Description": "text", "Speakers": "text"})
+    df.to_sql('Agenda', users.db_conn, if_exists='append', index=False)
+    users.close()
+
+
+if __name__ == "__main__":
+    try:
+        filename = argv[1]
+        print("The name of the agenda excel file is", filename)
+        if (not os.path.isfile(filename)):
+            raise ValueError("The file does not exist.")
+        pass
+    except Exception:
+        print("Please double check the input.")
+        raise
+
+Parseexcel(filename)
+
+
